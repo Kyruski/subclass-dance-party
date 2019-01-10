fcc4db2418380baab8c5960cf22a9325c1ec5135
Kyruski <jamescboyett@gmail.com>
2019-01-09 19:36:27 -0600

    Implements functionality of adding blinky dancers to the DOM;

--- src/dancer.js
+++ src/dancer.js
@@ -4,16 +4,16 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
-
+  
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 };
 
 MakeDancer.prototype.step = function() {
-  console.log('miosdidisdds ', this);
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
+
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 MakeDancer.prototype.setPosition = function(top, left) {
@@ -21,8 +21,8 @@ MakeDancer.prototype.setPosition = function(top, left) {
   // where it belongs on the page. See http://api.jquery.com/css/
   //
   var styleSettings = {
-    top: this.top,
-    left: this.left
+    top: top,
+    left: left
   };
   this.$node.css(styleSettings);
 };
--- src/init.js
+++ src/init.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   window.dancers = [];
-
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
@@ -20,9 +19,9 @@ $(document).ready(function() {
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
+    // make a dancer with a random positionß
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
